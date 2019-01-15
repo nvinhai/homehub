dd2c3b848883eab9b8d5cf9bafa150bdcdacb5a3
RYMND <kangrt95@gmail.com>
2019-01-15 03:23:59 -0500

    restart front end
    had capstone friend explain how react works, build front end from scratch this time

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,19 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import AppRouter from './routes/AppRouter';
-import { Provider } from 'react-redux';
-import configureStore from './store/store'
+import './index.css';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const store = configureStore();
-
-const jsx = (
-  <Provider store={store}>
-    <AppRouter/>
-  </Provider>
-)
-
-ReactDOM.render(jsx, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
